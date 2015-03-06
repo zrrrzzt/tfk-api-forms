613231ec1e4ac16ec1b0a5b5de8a1b44378b00ae
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-03-07 00:53:14 +0100

    Initial commit

--- handlers.js
+++ handlers.js
@@ -5,4 +5,45 @@ function getTemplate(request, reply) {
   reply('Heres a template');
 }
 
+function putTemplate(request, reply) {
+  console.log(request.params.templateId);
+  reply('Oooohhhh, new template');
+}
+
+function deleteTemplate(request, reply) {
+  console.log(request.params);
+  reply('Oooohhhh, This template is so gone');
+}
+
+function getTemplates(request, reply) {
+  console.log(request.params);
+  reply('Heres all the template');
+}
+
+function getForm(request, reply) {
+  console.log(request.params.formId);
+  reply('Heres the form');
+}
+
+function putForm(request, reply) {
+  console.log(request.params.formId);
+  reply('Oooohhhh, new efrm added');
+}
+
+function deleteForm(request, reply) {
+  console.log(request.params);
+  reply('Oooohhhh, This template is so gone');
+}
+
+function getForms(request, reply) {
+  console.log(request.params);
+  reply('Heres all the forms');
+}
+
 module.exports.getTemplate = getTemplate;
+
+module.exports.putTemplate = putTemplate;
+
+module.exports.deleteTemplate = deleteTemplate;
+
+module.exports.getTemplates = getTemplates;
